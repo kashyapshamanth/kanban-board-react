4037a4fe945da4aa145a7a03997a674dabb52583
Shamanth Kashyap N <shamanthkashyapn@Shamanths-MacBook-Air.local>
2025-11-18 15:05:41 +0530

    Fix duplicate React entry in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
